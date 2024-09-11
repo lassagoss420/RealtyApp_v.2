09570bc5d80ddc43013ddaf7ac1216852498b2e3
lassagoss420 <workmail.alex890@gmail.com>
2024-09-11 19:01:08 +0300

    added views, html base and home and css for base

--- RealtyAppv2/realtyApp/base/views.py
+++ RealtyAppv2/realtyApp/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import reverse
+from django.template.loader import get_template
 
-# Create your views here.
+
+def home(request):
+    print("TEST - Homepage!")
+
+    return render(request, template_name='home.html')
+
