1064588a214b42160308021846f6269bb3bab29e
lassagoss420 <workmail.alex890@gmail.com>
2024-09-12 21:07:12 +0300

    coded CSS for the nav bar, next is the MAP CARD, Listing Cards, and the logic behind

--- RealtyAppv2/realtyApp/base/urls.py
+++ RealtyAppv2/realtyApp/base/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('listings/', views.home, name='home'),
+    path('contact/', views.home, name='home'),
+    path('buy/', views.home, name='home'),
+    path('rent/', views.home, name='home'),
+    path('sell/', views.home, name='home'),
 ]
