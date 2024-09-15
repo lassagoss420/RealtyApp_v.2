d463adb08e0d10e3055d8c053bf02f80a5d56a3d
lassagoss420 <workmail.alex890@gmail.com>
2024-09-15 21:17:46 +0300

    added logic to convert addresses to lat and long coords, modded default save button to run convert_to_coord method and created Coordinates model to store coordinates in lat and long and to have a OTO rel with listings, also added the OSM map in views, next on TODO completing the map and rest of views, adding react and pairing it through vite and writing the front end

--- RealtyAppv2/realtyApp/base/models.py
+++ RealtyAppv2/realtyApp/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from geopy.geocoders import Nominatim
 
 
 class Client(models.Model):
@@ -30,14 +31,17 @@ class Listing(models.Model):
         get_latest_by = "order_date"
 
     CATEGORY = (
-        ('Buy', 'Buy'),
+        ('Buy', 'Buy'),  #maybe delete this? :) faulty logic
         ('Sell', 'Sell'),
         ('Rent', 'Rent'),
         )
 
     category = models.CharField(max_length=255, null=True, choices=CATEGORY)
     title = models.CharField(max_length=255, null=True, blank=True)
-    str_addr = models.CharField(max_length=255, null=True, blank=True, verbose_name='Street Address')
+    price = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True, verbose_name='City')
+    str_addr = models.CharField(max_length=255, null=True, blank=True, verbose_name='Street Name')
+    str_no = models.CharField(max_length=10, null=True, blank=True, verbose_name='Street Number')
     description = models.CharField(max_length=255, null=True, blank=True)
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)
 
@@ -49,3 +53,41 @@ class Listing(models.Model):
 
     def get_client_name(self):
         return self.client.name
+
+    def convert_to_coord(self):
+        geolocator = Nominatim(user_agent="realty_app")
+        full_address = f'{self.city}, {self.str_no}, {self.str_addr}' ##full address variable composition OSM uses city-str_no-str_name order, HAS to be dict|str not a tuple :( spent 10 mins to realise i need {}
+        coordinates = geolocator.geocode(full_address)
+
+        if coordinates:  # checks if coordinates already exist for this listing
+            latitude, longitude = coordinates.latitude, coordinates.longitude
+
+            if hasattr(self, 'coordinates'): # updates existing coordinates
+
+                self.coordinates.latitude = latitude
+                self.coordinates.longitude = longitude
+                self.coordinates.save()
+            else:
+                Coordinates.objects.create(  # creates new coordinates record
+                    listing=self,
+                    latitude=latitude,
+                    longitude=longitude
+                )
+            return True  # Coordinates saved successfully
+        else:
+            return False  # Could not find coordinates
+
+    def save(self, *args, **kwargs): #saves the listing instance
+        super().save(*args, **kwargs)
+        self.convert_to_coord() #added func to asave button when saving an existing or new listing it runs above method and saves coords, lat and long, in coordinates db_table
+        print('coords saved') # test
+
+
+class Coordinates(models.Model):
+    class Meta:
+        db_table = 'coordinates' #creates the db table coordinates to store data below
+
+    listing = models.OneToOneField(Listing, on_delete=models.CASCADE, related_name='coordinates', null=True)  #one to one rel with listings model, one set of coords linked to one full address
+    latitude = models.FloatField(null=True, blank=True)  #stores latitude in a Floatfield
+    longitude = models.FloatField(null=True, blank=True)  #stores longitude in a Floatfield
+
--- RealtyAppv2/realtyApp/base/views.py
+++ RealtyAppv2/realtyApp/base/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import reverse
 from django.template.loader import get_template
+from .models import Coordinates, Listing
+import folium
 
 
 def home(request):
+    # rendering and creating the homepage with the map
     print("TEST - Homepage!")
 
-    return render(request, template_name='home.html')
+    m = folium.Map(location=[19, -12], zoom_start=2) #creating the map object, need to automate passing the coords
+    m = m._repr_html_() #html representation for the map
+    context = {
+        'm': m,
+    }
+    return render(request, template_name='home.html', context=context)
+
 
